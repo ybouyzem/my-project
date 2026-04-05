04f6064f166a3785fc6c94eedc7ad9c0221b5401
ybouyzem <bouyzemyounes1@gmail.com>
2026-04-05 22:41:38 +0300

    Version 3: add multiply() and divide()

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-def add(a, b):      return a + b
-def subtract(a, b): return a - b
-print(add(3,5))
-print(subtract(10,4))
+def add(a,b):      return a + b
+def subtract(a,b): return a - b
+def multiply(a,b): return a * b
+def divide(a,b):   return a / b if b != 0 else 'Error'
